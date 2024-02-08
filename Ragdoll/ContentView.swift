79b0aee8f254076b940c80bb56e5683bf10d16be
茶茶 <chachakko@outlook.com>
2024-02-08 20:17:00 +0800

    chore: initial commit

---
+++ Ragdoll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ragdoll
+//
+//  Created by 茶茶 on 2024/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ragdoll/RagdollApp.swift
@@ -0,0 +1,17 @@
+//
+//  RagdollApp.swift
+//  Ragdoll
+//
+//  Created by 茶茶 on 2024/2/8.
+//
+
+import SwiftUI
+
+@main
+struct RagdollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
